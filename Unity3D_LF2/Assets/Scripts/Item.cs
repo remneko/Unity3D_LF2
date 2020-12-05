a8c219e6c094c57d7bd0952505f4bd8f57cde163
HWChia <66512878+xmas21@users.noreply.github.com>
2020-12-05 14:16:41 +0800

    人物場景修正 | 背包超細微腳本

---
+++ Unity3D_LF2/Assets/Scripts/Item.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+public class Item : MonoBehaviour
+{
+    [Header("武器圖片")]
+    public Sprite icon ;
+    [Header("武器數量")]
+    public int count ;
+    [Header("武器分類")]
+    public int ItemTag ;
+    [Header("是否裝備")]
+    public bool Equip = false;
+}
